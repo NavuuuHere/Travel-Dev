6a757c4851a29a9011eb1b2629035cc65a5485c1
Saurashya <Saurashya1@gmail.com>
2023-08-01 12:31:17 +0545

    Debugged pricing.js,SAM

--- pricing.js
+++ pricing.js
@@ -1,36 +1,33 @@
 const btns1 = document.querySelectorAll(".nav-btn-sam1");
-const slides1 = document.querySelectorAll(".card-pr");
-let currentSlideIndex1 = 0; 
-let autoplayIntervalId1; 
+  const slides1 = document.querySelectorAll(".card-pr");
+  let currentSlideIndex1 = 0;
+  let autoplayInterval_sam; // Variable to store the autoplay interval
 
-var sliderNav1 = function (manual) {
-  btns1.forEach((btn1, i1) => {
-    btn1.classList.remove("active");
-  });
-  slides1.forEach((slide1, i1) => {
-    slide1.classList.remove("active");
-  });
-  btns1[manual].classList.add("active");
-  slides1[manual].classList.add("active");
-  currentSlideIndex1 = manual; // Update the current slide index after navigation
-  resetAutoplayInterval(); // Reset the autoplay interval after manual slide change
-};
-const totalSlides1 = slides1.length;
-const autoplayInterval1 = 10000; // Set the time interval for autoplay (in milliseconds)
+  const sliderNav1 = function (manual) {
+    btns1.forEach((btn1, i) => {
+      btn1.classList.remove("active");
+    });
+    slides1.forEach((slide1, i) => {
+      slide1.classList.remove("active");
+    });
+    btns1[manual].classList.add("active");
+    slides1[manual].classList.add("active");
+    currentSlideIndex1 = manual; // Update the current slide index after navigation
+  };
 
-const autoplaySlides1 = () => {
-  // Calculate the next slide index
-  const nextSlideIndex1 = (currentSlideIndex1 + 1) % totalSlides1;
-  sliderNav1(nextSlideIndex1);
-};
-btns1.forEach((btn1, i1) => {
-  btn1.addEventListener("click", () => {
-    sliderNav1(i1);
+  btns1.forEach((btn1, i) => {
+    btn1.addEventListener("click", () => {
+      sliderNav1(i);
+      clearInterval(autoplayInterval_sam); // Reset autoplay interval when user manually navigates
+    });
   });
-});
 
-const resetAutoplayInterval = () => {
-  clearInterval(autoplayIntervalId1);
-  autoplayIntervalId1 = setInterval(autoplaySlides1, autoplayInterval1);
-};
-autoplayIntervalId1 = setInterval(autoplaySlides1, autoplayInterval1);
+  const autoplay = function () {
+    currentSlideIndex1 = (currentSlideIndex1 + 1) % slides1.length; // Move to the next slide
+    sliderNav1(currentSlideIndex1);
+  };
+
+  // Adjust the autoplay timing here (in milliseconds)
+  const autoplayDelay = 10000; // 5 seconds
+
+  autoplayInterval_sam = setInterval(autoplay, autoplayDelay); // Start the autoplay
